0170d4e4ea318930e20cc4f72ef4328835511eef
Luciano Tavernard <lucianotavernard@gmail.com>
2022-07-21 00:57:32 -0300

    feat: adjust task component

--- src/components/Task.tsx
+++ src/components/Task.tsx
@@ -1,19 +1,31 @@
 import { Check, Trash } from 'phosphor-react'
 
 type TaskProps = {
+  id: number
   title: string
   isComplete?: boolean
+  onFinish: (id: number) => void
+  onRemove: (id: number) => void
 }
 
-export function Task({ title, isComplete }: TaskProps) {
+export function Task({ id, title, isComplete, onFinish, onRemove }: TaskProps) {
+  function markTaskAsCompleted() {
+    onFinish(id)
+  }
+
+  function removeTask() {
+    onRemove(id)
+  }
+
   return (
     <article className="flex justify-between items-start p-4 border border-[#333333] rounded-lg bg-[#262626]">
       <button
         type="button"
+        onClick={markTaskAsCompleted}
         className={
           isComplete
-            ? 'flex justify-center items-center w-5 h-5 border-0 rounded-full bg-[#5E60CE] text-[#F2F2F2] cursor-pointer hover:opacity-95'
-            : 'flex justify-center items-center w-5 h-5 border-2 border-[#4ea8de] rounded-full cursor-pointer hover:opacity-95'
+            ? 'transition-all flex justify-center items-center w-5 h-5 border-0 rounded-full bg-[#5E60CE] text-[#F2F2F2] cursor-pointer hover:opacity-95'
+            : 'transition-all flex justify-center items-center w-5 h-5 border-2 border-[#4ea8de] rounded-full hover:bg-[#1E6F9F] hover:opacity-20 cursor-pointer'
         }
       >
         {isComplete && <Check size={12} />}
@@ -22,16 +34,20 @@ export function Task({ title, isComplete }: TaskProps) {
       <p
         className={
           isComplete
-            ? 'w-[90%] ml-3 text-sm text-[#F2F2F2] line-through'
-            : 'w-[90%] ml-3 text-sm text-[#F2F2F2]'
+            ? 'transition-all w-[90%] ml-3 text-sm text-[#808080] line-through'
+            : 'transition-all w-[90%] ml-3 text-sm text-[#F2F2F2]'
         }
       >
         {title}
       </p>
 
-      <button type="button" className="hover:opacity-95 text-white">
+      <button
+        type="button"
+        onClick={removeTask}
+        className="transition-all flex justify-center items-center w-8 h-8 rounded bg-transparent hover:bg-[#333333] text-white hover:opacity-95"
+      >
         <Trash
-          className="text-[#808080] hover:text-[#e25858] duration-75"
+          className="transition-all text-[#808080] hover:text-[#e25858] "
           size={20}
         />
       </button>
